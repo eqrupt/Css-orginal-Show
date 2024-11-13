a0fea80dc6c9f045be3ed57af0d4ee6597207097
eqrupt <enoughdictionary@163.com>
2024-11-13 08:29:08 +0800

    add_mainpage

--- Css-vue-project/src/App.vue
+++ Css-vue-project/src/App.vue
@@ -4,23 +4,76 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div id="app" class="d-flex">
+    <!-- 侧边栏 -->
+    <div class="sidebar p-3">
+      <h5>网易云音乐</h5>
+      <ul class="list-unstyled">
+        <li><a href="#">推荐</a></li>
+        <li><a href="#">精选</a></li>
+        <li><a href="#">播客</a></li>
+        <li><a href="#">漫游</a></li>
+        <li><a href="#">动态</a></li>
+      </ul>
+      <hr>
+      <h5>我的</h5>
+      <ul class="list-unstyled">
+        <li><a href="#">喜欢的音乐</a></li>
+        <li><a href="#">最近播放</a></li>
+        <li><a href="#">我的播客</a></li>
+      </ul>
+      <hr>
+      <h5>创建的歌单</h5>
+      <ul class="list-unstyled">
+        <li><a href="#">下山</a></li>
+        <li><a href="#">泡泡的2023年度歌单</a></li>
+      </ul>
     </div>
-  </header>
 
-  <RouterView />
+    <!-- 主要内容区 -->
+    <div class="content p-4">
+      <h5>推荐歌单</h5>
+      <div class="row">
+        <div class="col-6 col-md-4">
+          <div class="card mb-4">
+            <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">每日推荐</h5>
+              <p class="card-text">从「Couch Potato」...</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-6 col-md-4">
+          <div class="card mb-4">
+            <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">私人雷达</h5>
+              <p class="card-text">今天《Chatterbox》...</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h5>榜单精选</h5>
+      <!-- 此处可以添加其他内容 -->
+    </div>
+  </div>
 </template>
 
 <style scoped>
+#app {
+  display: flex;
+  height: 100vh;
+}
+
+.sidebar {
+  width: 200px;
+  background-color: #f8f9fa;
+}
+
+.content {
+  flex: 1;
+  overflow-y: auto;
+}
 header {
   line-height: 1.5;
   max-height: 100vh;
--- Css-vue-project/src/views/AboutView.vue
+++ Css-vue-project/src/views/AboutView.vue
@@ -10,6 +10,6 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
-  }
+  } 
 }
 </style>
